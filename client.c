cd1539d3bd1c95cc85658bec0b148d519e5a0e99
Julia Oghigian <oghigian@snares-08.cs.wisc.edu>
2022-12-08 15:30:53 -0600

    error cases

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "udp.h"
+#include "mfs.h"
 
 #define BUFFER_SIZE (1000)
 
@@ -7,21 +7,21 @@
 int main(int argc, char *argv[]) {
     struct sockaddr_in addrSnd, addrRcv;
 
-    int sd = UDP_Open(20000);
-    int rc = UDP_FillSockAddr(&addrSnd, "localhost", 10000);
+    int sd = MFS_Init(20000);
+    int rc = UDP_FillSockAddr(&addrSnd, "localhost", 10000);//not right
 
     char message[BUFFER_SIZE];
     sprintf(message, "hello world");
 
     printf("client:: send message [%s]\n", message);
-    rc = UDP_Write(sd, &addrSnd, message, BUFFER_SIZE);
+    rc = MFS_Write(sd, &addrSnd, message, BUFFER_SIZE);
     if (rc < 0) {
 	printf("client:: failed to send\n");
 	exit(1);
     }
 
     printf("client:: wait for reply...\n");
-    rc = UDP_Read(sd, &addrRcv, message, BUFFER_SIZE);
+    rc = MFS_Read(sd, &addrRcv, message, BUFFER_SIZE);
     printf("client:: got reply [size:%d contents:(%s)\n", rc, message);
     return 0;
 }
